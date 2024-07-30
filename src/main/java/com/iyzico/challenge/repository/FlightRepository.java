251f8dc2ad900a9f30a90a855303ad96d9516946
furkanilhan <furkan7481@gmail.com>
2024-07-31 02:28:13 +0300

    Removing ignored files

--- src/main/java/com/iyzico/challenge/repository/FlightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iyzico.challenge.repository;
-
-import com.iyzico.challenge.entity.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-}
--- src/main/java/com/iyzico/challenge/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iyzico.challenge.repository;
-
-import com.iyzico.challenge.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
